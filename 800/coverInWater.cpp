68b1b51fcb3e22e0e5880ae8285f322235c0a548
capric0jatin <jatingtrajput009@gmail.com>
2024-06-14 19:17:19 +0530

    cinw

---
+++ 800/coverInWater.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int h=0;
+        int dot=0;
+        int tot=0;
+        int m=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='.')
+            {
+                dot++;
+                tot++;
+            }else{
+                dot=0;
+            }
+            m=max(m,dot);
+        }
+        if(m>2)
+        {
+            cout<<2<<endl;
+        }else{
+            cout<<tot<<endl;
+        }
+    }
+}
