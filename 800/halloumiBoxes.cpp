229cfab3372dab695da100694f32a66e5ef3d22c
capric0jatin <jatingtrajput009@gmail.com>
2024-06-14 15:34:48 +0530

    cp

---
+++ 800/halloumiBoxes.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        vector<int> v;
+        for(int i=0;i<n;i++)
+        {
+            int num;
+            cin>>num;
+            v.push_back(num);
+        }
+        if(is_sorted(v.begin(),v.end()) || k>=2)
+        {
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
