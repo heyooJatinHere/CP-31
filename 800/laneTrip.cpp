ccefa2fe74a5a677a09452493f97f32b3c857371
capric0jatin <jatingtrajput009@gmail.com>
2024-06-14 15:51:50 +0530

    ltrip

---
+++ 800/laneTrip.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        vector<int> v;
+        for(int i=0;i<n;i++)
+        {
+            int num;
+            cin>>num;
+            v.push_back(num);
+        }
+        int m=v[0];
+        if(n==1)
+        {
+            cout<<m<<endl;
+        }else{
+            for(int i=1;i<n;i++)
+            {
+                m=max(m,v[i]-v[i-1]);
+            }
+            m=max(m,2*(x-v[n-1]));
+            cout<<m<<endl;
+        }
+
+
+    }
+}
